abbbaa1afab2e9e3b5459b0cb94f9b14832465dc
Flavio Atas Medeiros <fatasmedeiros@deloitte.nl>
2020-11-11 15:59:28 +0100

    Co-authored-by: van den Berg, Bob <bovandenberg@deloitte.nl>

--- src/App.js
+++ src/App.js
@@ -1,61 +1,13 @@
-import { useRef } from "react";
 import "./App.css";
 
-import RecordRTC, { StereoAudioRecorder } from "recordrtc";
-
-var recorder; // globally accessible
+import recorder from "./recorder";
 
 function App() {
-  // const audio = useRef();
-  const audio = new Audio(); // controls autoPlay playsInline
-
-  function captureMicrophone(callback) {
-    navigator.mediaDevices
-      .getUserMedia({ audio: true })
-      .then(callback)
-      .catch(function (error) {
-        alert("Unable to access your microphone.");
-        console.error(error);
-      });
-  }
-
-  function stopRecordingCallback() {
-    audio.srcObject = null;
-    var blob = recorder.getBlob();
-    audio.src = URL.createObjectURL(blob);
-
-    recorder.microphone.stop();
-
-    // audio.play();
-  }
-
-  function startRecording() {
-    // this.disabled = true;
-    captureMicrophone(function (microphone) {
-      audio.srcObject = microphone;
-
-      recorder = RecordRTC(microphone, {
-        type: "audio",
-        recorderType: StereoAudioRecorder,
-        desiredSampRate: 16000,
-      });
-
-      recorder.startRecording();
-
-      // release microphone on stopRecording
-      recorder.microphone = microphone;
-
-      // document.getElementById("btn-stop-recording").disabled = false;
-    });
-  }
-
   return (
     <div className="App">
       {/* <audio ref={audio} controls autoPlay playsInline></audio> */}
-      <button onClick={() => startRecording()}>Start recording</button>
-      <button onClick={() => recorder.stopRecording(stopRecordingCallback)}>
-        Stop recording
-      </button>
+      <button onClick={() => recorder.start()}>Start recording</button>
+      <button onClick={() => recorder.stop()}>Stop recording</button>
     </div>
   );
 }
---
+++ src/recorder.js
@@ -0,0 +1,55 @@
+import RecordRTC, { StereoAudioRecorder } from "recordrtc";
+
+var recorder; // globally accessible
+
+const audio = new Audio();
+
+function captureMicrophone(callback) {
+  navigator.mediaDevices
+    .getUserMedia({ audio: true })
+    .then(callback)
+    .catch(function (error) {
+      alert("Unable to access your microphone.");
+      console.error(error);
+    });
+}
+
+function stopRecordingCallback() {
+  audio.srcObject = null;
+  var blob = recorder.getBlob();
+  audio.src = URL.createObjectURL(blob);
+
+  recorder.microphone.stop();
+
+  audio.play();
+}
+
+function startRecording() {
+  // this.disabled = true;
+  captureMicrophone(function (microphone) {
+    audio.srcObject = microphone;
+
+    recorder = RecordRTC(microphone, {
+      type: "audio",
+      recorderType: StereoAudioRecorder,
+      desiredSampRate: 16000,
+    });
+
+    recorder.startRecording();
+
+    // release microphone on stopRecording
+    recorder.microphone = microphone;
+
+    // document.getElementById("btn-stop-recording").disabled = false;
+  });
+}
+
+function start() {
+  startRecording();
+}
+
+async function stop() {
+  recorder.stopRecording(stopRecordingCallback);
+}
+
+export default { start, stop };
