971236e40f66c4250671fb4b0cf5edd87cf5217a
Flavio Atas Medeiros <fatasmedeiros@deloitte.nl>
2020-11-11 15:46:16 +0100

    Working!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import "./App.css";
+
+import RecordRTC, { StereoAudioRecorder } from "recordrtc";
+
+var recorder; // globally accessible
 
 function App() {
+  const audio = useRef();
+
+  function captureMicrophone(callback) {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then(callback)
+      .catch(function (error) {
+        alert("Unable to access your microphone.");
+        console.error(error);
+      });
+  }
+
+  function stopRecordingCallback() {
+    audio.current.srcObject = null;
+    var blob = recorder.getBlob();
+    audio.current.src = URL.createObjectURL(blob);
+
+    recorder.microphone.stop();
+  }
+
+  function startRecording() {
+    // this.disabled = true;
+    captureMicrophone(function (microphone) {
+      audio.current.srcObject = microphone;
+
+      recorder = RecordRTC(microphone, {
+        type: "audio",
+        recorderType: StereoAudioRecorder,
+        desiredSampRate: 16000,
+      });
+
+      recorder.startRecording();
+
+      // release microphone on stopRecording
+      recorder.microphone = microphone;
+
+      // document.getElementById("btn-stop-recording").disabled = false;
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <audio ref={audio} controls autoPlay playsInline></audio>
+      <button onClick={() => startRecording()}>Start recording</button>
+      <button onClick={() => recorder.stopRecording(stopRecordingCallback)}>
+        Stop recording
+      </button>
     </div>
   );
 }
